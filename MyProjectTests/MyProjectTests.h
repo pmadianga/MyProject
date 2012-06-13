ece714f8cfb2302db501b688262920f625ec2970
Prudence Madianga <prudence.madianga.sii@adp.fr>
2012-06-13 14:39:33 +0200

    initialisation du projet

---
+++ MyProjectTests/MyProjectTests.h
@@ -0,0 +1,13 @@
+//
+//  MyProjectTests.h
+//  MyProjectTests
+//
+//  Created by mac_serveur_adp_1 on 13/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MyProjectTests : SenTestCase
+
+@end
---
+++ MyProjectTests/MyProjectTests.m
@@ -0,0 +1,32 @@
+//
+//  MyProjectTests.m
+//  MyProjectTests
+//
+//  Created by mac_serveur_adp_1 on 13/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MyProjectTests.h"
+
+@implementation MyProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MyProjectTests");
+}
+
+@end
